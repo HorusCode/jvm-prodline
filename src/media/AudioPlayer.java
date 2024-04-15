fc17269adadc292a03fbf7214efd5b0bb30dbbbe
Ильназ Фаттахов <i.fattakhov@sprinthost.ru>
2024-04-15 12:50:51 +0300

    Организована структура будущего проекта в соответствии UML

---
+++ src/media/AudioPlayer.java
@@ -0,0 +1,6 @@
+package media;
+
+import core.Product;
+
+public class AudioPlayer extends Product implements MultimediaControl {
+}
---
+++ src/media/MoviePlayer.java
@@ -0,0 +1,6 @@
+package media;
+
+import core.Product;
+
+public class MoviePlayer extends Product implements MultimediaControl {
+}
